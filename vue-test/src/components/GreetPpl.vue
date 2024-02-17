78d3c68a873442cca7459422c992c7f62a4d6017
markk87 <markk87@nycstudents.net>
2024-02-17 10:56:23 -0500

    asdfasfd

---
+++ vue-test/src/components/GreetPpl.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2 class="header">Hello Mark</h2>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: "Greet"
+}
+</script>
+
+<style scoped>
+.header {
+    
+    font-family: 'Times New Roman', Times, serif;
+}
+</style>
